b414555297d3dca29213435380e8a9f75fd22528
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-07 23:44:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,7 +48,7 @@ class Dog
   end 
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
+    sql = "SELECT * FROM dogs WHERE name = ?"
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
 end
