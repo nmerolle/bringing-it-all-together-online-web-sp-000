aa9d61bef095fc2ae68125716a755bfa1795f155
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-07 22:39:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,17 @@ class Dog
     self.id ||= nil
   end
 
-
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+ 
 
 end
 
