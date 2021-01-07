db963a32b02293d3f97a06d0c2149639ac161f29
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-07 23:43:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,16 @@ class Dog
     dog
   end
   
-  
+    def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+  end 
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
 end
 
 
