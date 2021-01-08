02c75275affce4992f51df804cf389164f66d048
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-08 00:21:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,17 +59,22 @@ class Dog
     end.first
   end
 
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
 
 
+      dog = DB[:conn].execute(sql, name, breed).first
 
-
-
-
-
-
-
-
-
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
