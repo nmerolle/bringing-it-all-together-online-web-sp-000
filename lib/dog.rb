d59a9a785e56b789776af4f73cc5f616ecb071bd
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-08 00:17:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,10 @@ class Dog
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
   
-  
+    def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
 
 
